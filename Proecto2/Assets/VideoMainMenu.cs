a3b11ff1f12b8cd5893ba29c9d8420d437764025
Oriol Calvis Diaz <oriol.calvis@enti.cat>
2019-06-10 17:03:53 +0200

    Main Menu

---
+++ Proecto2/Assets/VideoMainMenu.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideoMainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float velocity = 10.0f;
+    public float timer = 0.0f;
+    public int i = 0;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        int seconds = (int)timer % 60;
+        this.transform.localPosition = new Vector3(this.transform.localPosition.x - (velocity * Time.deltaTime), this.transform.localPosition.y + ((-9.81f + Random.Range(2, 9) + 5) * Time.deltaTime), this.transform.localPosition.z + (velocity * Time.deltaTime));
+        this.transform.localRotation = new Quaternion(0.0f, -0.997993f, 0.0f, 0.06332435f);
+        Debug.Log(seconds);
+
+        if (seconds == 15 * i)
+        {
+            this.transform.localRotation = new Quaternion(0.0f, -0.997993f, 0.0f, 0.06332435f);
+            this.transform.localPosition = new Vector3(164, 0.0f, -219);
+            i++;
+        }
+
+    }
+}
